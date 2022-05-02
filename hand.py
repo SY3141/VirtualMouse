c9361a02297dc8af6dfc0e86636865b2d9c10c9b
SunnyY15926 <45080122+SunnyY15926@users.noreply.github.com>
2022-05-02 17:12:16 -0400

    Added mouse acceleration
    works in mouse centered games like aimlabs and minecraft

--- hand.py
+++ hand.py
@@ -1,142 +1,191 @@
 import time
 import cv2
 import mediapipe as mp
+from numpy import newaxis
 from pynput.mouse import Button, Controller
+import win32api, win32con
 
 
 class Hand:
-    def __init__(self, drawHand=False, moveThresh=20, suppressShake=True, frameSample=5):
-        self.drawHand = drawHand  # boolean: draws connections of the hand landmarks
+    def __init__(self, drawLabels=False, moveThresh=0, suppressShake=True, frameSample=3):
+        self.drawLabels = drawLabels  # boolean: draws connections of the hand landmarks
         self.moveThresh = moveThresh
         self.suppressShake = suppressShake
-        self.mouseDown = False
-        self.display = (1920, 1080)  # (1920,1080) self.display
+
+
+        self.display = (1920, 1080)  # (1920,1080) resolution
+        self.boundStart = (40,40) #(320,40)
+        self.boundBox = (560,400) #(280,400)
+        
         self.tipIds = [4, 8, 12, 16, 20]
         self.fingersRaised = [0, 0, 0, 0, 0]
-        self.handPositions = [[0, 0] for i in range(frameSample)]
         self.pTime = 0
-        self.prevX = 0
-        self.prevY = 0
-        self.handX = 0
-        self.handY = 0
+
+        self.frameRate = [0 for i in range(5)]
+        self.prevInput = (0,0)
+        self.acceleration = 1.5
+        self.mouseCoords = (self.display[0]/2,self.display[1]/2)
+        self.lastPos = [(0, 0) for i in range(frameSample)]
 
         self.mouse = Controller()
         self.clickThresh = 0.5
         self.lastClick = 0
-        self.running = True
+        self.mouseRunning = True
+        self.leftDown = False
+        self.rightDown = False
+        self.mouseAction = "None"
 
-        self.cap = cv2.VideoCapture(0)
         self.mpHands = mp.solutions.hands
-        self.hands = self.mpHands.Hands(False, 1, False, 0.5, 0.5)
+        self.hands = self.mpHands.Hands(False, 1, False, 0.5, 0.5) 
         self.mpDraw = mp.solutions.drawing_utils
+        self.cap = cv2.VideoCapture(0)
+
+    def release(self):
+        self.cap.release()
+        cv2.destroyAllWindows()
+
+    def setAcc(self, acceleration):
+        self.acceleration = acceleration
 
     def avg(self, lst):
         if len(lst) > 0:
             return sum(lst) / len(lst)
-
-    def avgSlope(self, lst):  # input of a matrix of x,y positions
-        deltaX = []
-        deltaY = []
-        for i in range(len(lst)):
-            if i != len(lst) - 1:
-                deltaX.append(lst[i+1][0] - lst[i][0])
-                deltaY.append(lst[i+1][1] - lst[i][1])
-
-        avgX = self.avg(deltaX)
-        avgY = self.avg(deltaY)
-        #print("Slopes: ", [avgX, avgY])
-        return [avgX, avgY]
-
+            
     def avgPos(self, lst):  # input of a matrix of x,y positions
         avgX = self.avg([x[0] for x in lst])
         avgY = self.avg([y[1] for y in lst])
         return [int(avgX), int(avgY)]
 
     def queue(self, lst, point):
-        newQueue = []
-        for i in range(1, len(lst)):
-            newQueue.append(lst[i])
+        newQueue = lst[1:]
         newQueue.append(point)
         return newQueue
+    
+    def overThresh(self,startCoords, endCoords):
+        if abs(startCoords[0] - endCoords[0]) > self.moveThresh or abs(startCoords[1] - endCoords[1]) > self.moveThresh:
+            return True
+        else:
+            return False
 
-    def suppressMotion(self, handLms, inverted=True, xScale=2, yScale=2, handOffsetX=400, handOffsetY=400):
-        input_X = self.avg([handLms.landmark[item].x for item in [0, 9, 13]])
-        input_Y = self.avg([handLms.landmark[item].y for item in [0, 9, 13]])
-        self.handX = int(input_X * (self.display[0] * xScale) - handOffsetX)
-        self.handY = int(input_Y * (self.display[1] * yScale) - handOffsetY)
+    def mouseAcceleration(self, handLms, camSize, img):
+        inputX = int(self.avg([handLms.landmark[item].x for item in [0, 9, 13]]) * camSize[0])
+        inputY = int(self.avg([handLms.landmark[item].y for item in [0, 9, 13]]) * camSize[1])
+        if (inputX > self.boundStart[0] and inputX < self.boundStart[0] + self.boundBox[0] 
+        and inputY > self.boundStart[1] and inputY < self.boundStart[1] + self.boundBox[1]):
+            self.inBounds = True
+            moveX = (inputX - self.prevInput[0]) * 2
+            moveY = (inputY - self.prevInput[1]) * 2
+            
+            if moveX < 0:
+                moveX = -(abs(moveX) ** self.acceleration)
+            else:
+                moveX = moveX ** self.acceleration
+            if moveY < 0:
+                moveY = -(abs(moveY) ** self.acceleration)
+            else:
+                moveY = moveY ** self.acceleration
+            
+            mouseX = self.mouseCoords[0] + moveX
+            mouseY = self.mouseCoords[1] + moveY
+            if mouseX > self.display[0]:
+                mouseX = self.display[0]
+            if mouseX < 0:
+                mouseX = 0
+            if mouseY > self.display[1]:
+                mouseY = self.display[1]
+            if mouseY < 0:
+                mouseY = 0
+            self.mouseCoords = (mouseX, mouseY)
+            self.lastPos = self.queue(self.lastPos, self.mouseCoords)
+            avgPos = self.avgPos(self.lastPos)
 
-        if inverted:
-            # inverted for camera facing you
-            self.handX = self.display[0] - self.handX
-
-        if self.suppressShake:
-            if abs(self.handX-self.prevX) > self.moveThresh or abs(self.handY - self.prevY) > self.moveThresh:  # suppresses shaking
-                self.prevX = self.handX
-                self.prevY = self.handY
-
-            self.handPositions = self.queue(
-                self.handPositions, [self.handX, self.handY])
-            self.handX = self.avgPos(self.handPositions)[0]
-            self.handY = self.avgPos(self.handPositions)[1]
+            if self.overThresh(self.mouseCoords, avgPos):
+                x =  int(self.mouseCoords[0] - avgPos[0])
+                y =  int(self.mouseCoords[1] - avgPos[1])
+                win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0) #necessary for centered mouse programs
+                #self.mouse.move(x, y)
+                #self.mouse.position = avgPos
+                
+                self.prevInput = (inputX, inputY)
+        else:
+            self.inBounds = False
+        
+        cv2.circle(img, (inputX, inputY), 3, (255, 255, 0), -1)  # draws hand position
 
     def controlMouse(self):
         if self.fingersRaised[1:5] == [1,1,1,1]:# if 4 fingers, thumb not included, are raised
-            if self.mouseDown == False:
+            if self.leftDown == False:
                 self.mouse.press(Button.left)
-                self.mouseDown = True
-        elif self.fingersRaised[1:5] == [0,1,1,1]: # if 3 fingers, thumb not included and index not included, are raised
-            if self.mouseDown == False:
-                self.mouse.press(Button.right)
-                self.mouseDown = True
+                self.leftDown = True
+                self.mouseAction = "Left"
         else:
-            self.mouseDown = False
-            self.mouse.release(Button.left)
+            if self.leftDown == True:
+                self.leftDown = False
+                self.mouse.release(Button.left)
+                self.mouseAction = "None"
+        if self.fingersRaised[1:5] == [0,1,1,1]: # if 3 fingers, thumb not included and index not included, are raised
+            if self.rightDown == False:
+                self.mouse.press(Button.right)
+                self.rightDown = True
+                self.mouseAction = "Right"
+        else:
+            if self.rightDown == True:
+                self.rightDown = False
+                self.mouse.release(Button.right)
+                self.mouseAction = "None"
             # index, middle and ring finger raised
+            '''
             if self.fingersRaised[1:4] == [1, 1, 1]:
                 if self.handY <= self.display[1] / 2:
                     scroll = int((self.display[1]/2 - self.handY)/100)
                 else:
                     scroll = int(-(self.handY - self.display[1]/2)/100)
                 self.mouse.scroll(0, scroll)
+            '''
             # index and middle finger raised
-            elif self.fingersRaised[1:3] == [1, 1]:
+        if self.leftDown == False and self.rightDown == False:
+            if self.fingersRaised[1:3] == [1, 1]:
                 if (time.time() - self.lastClick) > self.clickThresh:
                     self.mouse.click(Button.right, 1)
+                    self.mouseAction = "Right Click"
                     self.lastClick = time.time()
             elif self.fingersRaised[1] == 1:  # index finger raised
                 if (time.time() - self.lastClick) > self.clickThresh:
                     self.mouse.click(Button.left, 1)
+                    self.mouseAction = "Left Click"
                     self.lastClick = time.time()
 
+
     def draw(self):
-        sucesss, img = self.cap.read()  # tuple of boolean success and image feed
+        success, img = self.cap.read()  # tuple of boolean success and image feed
+        img = cv2.flip(img,1) #inverts camera feed
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.hands.process(imgRGB)
         h, w, c = img.shape
         y_offset = 5  # vertical position offset for number label on finger landmarks
         if results.multi_hand_landmarks:
-            for handLms in results.multi_hand_landmarks:
-                self.mpDraw.draw_landmarks(
-                    img, handLms, self.mpHands.HAND_CONNECTIONS)
+            for handLms in results.multi_hand_landmarks: #iterating through each hand
+                #self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
                 for id, lm in enumerate(handLms.landmark):
-                    if self.drawHand:
-                        cv2.putText(img, str(id), (int(
-                            lm.x * w), int(lm.y * h - y_offset)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                    if self.drawLabels:
+                        cv2.putText(img, str(id), (int(lm.x * w), int(lm.y * h - y_offset)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                     if id % 4 == 0 and id > 4:
                         if lm.y < handLms.landmark[id - 2].y:
                             self.fingersRaised[id // 4 - 1] = 1
                         else:
                             self.fingersRaised[id // 4 - 1] = 0
-                self.suppressMotion(handLms)
-                self.mouse.position = (self.handX, self.handY)
-
-            self.controlMouse()
+                self.mouseAcceleration(handLms, (w, h), img)
+            if self.mouseRunning and self.inBounds:
+                self.controlMouse()
 
         cTime = time.time()
-        fps = round(1/(cTime - self.pTime), 2)
+        curFps = round(1/(cTime - self.pTime))
+        self.frameRate = self.queue(self.frameRate,curFps)
+        avgFps = "FPS: " + str(round(self.avg(self.frameRate)))
         self.pTime = cTime
 
-        cv2.putText(img, str(fps), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, (0, 255, 0), 2)  # displays fps
+        cv2.putText(img, avgFps, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays fps
+        cv2.putText(img, self.mouseAction, (400, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays fps
+        cv2.rectangle(img, self.boundStart,( self.boundStart[0] + self.boundBox[0] , self.boundStart[1] + self.boundBox[1]), (0, 0, 255), 2) #draws virtual mousepad area
         cv2.imshow('Camera1', img)  # shows camera feed
         cv2.waitKey(1)  # waits for 1ms
--- main.py
+++ main.py
@@ -3,30 +3,34 @@ from pynput.mouse import Button, Controller
 from pynput import keyboard
 import os
 
-hand1 = Hand(moveThresh = 10, frameSample = 10)
+hand1 = Hand()
 keySender = Controller()
+run = True
 
 def on_press(key):
     try:
         #print('alphanumeric key {0} pressed'.format(key.char))
-        if key.char == '/': #toggles pauses the program
-            hand1.running = not hand1.running
+        if key.char == '/':  # toggles pauses the program
+            hand1.mouseRunning = not hand1.mouseRunning
     except AttributeError:
         print('special key {0} pressed'.format(key))
 
+
 def on_release(key):
-    #print('{0} released'.format(key))
-    if key == keyboard.Key.esc: #stops the program
-        os._exit(0) 
+    print('{0} released'.format(key))
+    if key == keyboard.Key.esc:  # stops the program
+        os._exit(0)
+        hand1.release()
 
 listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
 
+
 def main():
     while True:
-        if hand1.running:
-            hand1.draw()
+        hand1.draw()
+
 
 main()
