e9d2c5a10eafe5dad49eb1fae0b48a3655daf343
SY <45080122+SunnyY15926@users.noreply.github.com>
2022-05-25 14:48:24 -0400

    worked on detecting pinch

--- hand.py
+++ hand.py
@@ -5,17 +5,17 @@ from numpy import newaxis
 from pynput.mouse import Button, Controller
 import win32api, win32con
 
-
 class Hand:
-    def __init__(self, drawLabels=False, moveThresh=0, suppressShake=True, frameSample=3):
-        self.drawLabels = drawLabels  # boolean: draws connections of the hand landmarks
+    def __init__(self, moveThresh=0, suppressShake=True, frameSample=3):
+        self.drawLabels = False  # boolean: draws connections of the hand landmarks
+        self.drawConnections = True
+        self.drawLandmarks = True
         self.moveThresh = moveThresh
         self.suppressShake = suppressShake
 
-
         self.display = (1920, 1080)  # (1920,1080) resolution
         self.boundStart = (40,40) #(320,40)
-        self.boundBox = (560,400) #(280,400)
+        self.boundBox = (560,350) #(280,400)
         
         self.tipIds = [4, 8, 12, 16, 20]
         self.fingersRaised = [0, 0, 0, 0, 0]
@@ -23,7 +23,7 @@ class Hand:
 
         self.frameRate = [0 for i in range(5)]
         self.prevInput = (0,0)
-        self.acceleration = 1.5
+        self.acceleration = 1.4
         self.mouseCoords = (self.display[0]/2,self.display[1]/2)
         self.lastPos = [(0, 0) for i in range(frameSample)]
 
@@ -34,18 +34,16 @@ class Hand:
         self.leftDown = False
         self.rightDown = False
         self.mouseAction = "None"
+        self.pinch = False
+        self.inBounds = True
 
-        self.thumbCoords = [0,0,0]
-        self.indexCoords = [0,0,0]
+        #self.thumbCoords = [0,0,0]
+        #self.indexCoords = [0,0,0]
 
         self.mpHands = mp.solutions.hands
         self.hands = self.mpHands.Hands(False, 1, False, 0.5, 0.5) 
         self.mpDraw = mp.solutions.drawing_utils
         self.cap = cv2.VideoCapture(0)
-
-    def release(self):
-        self.cap.release()
-        cv2.destroyAllWindows()
 
     def setAcc(self, acceleration):
         self.acceleration = acceleration
@@ -58,19 +56,35 @@ class Hand:
         avgX = self.avg([x[0] for x in lst])
         avgY = self.avg([y[1] for y in lst])
         return [int(avgX), int(avgY)]
+    
+    def secant(self,x1,y1,x2,y2):
+        dy = y1-y2
+        dx = x1-x2
+        return dy/dx
 
+    def dist(self,x1,y1, x2,y2):
+        dx = x1 - x2
+        dy = y1 - y2
+        return (dx**2 +dy**2) ** 0.5
     def queue(self, lst, point):
         newQueue = lst[1:]
         newQueue.append(point)
         return newQueue
     
-    def pinch(self, p1, p2):
-        xyDist = ((p1[0]-p1[1])**2 + (p2[0]-p2[1])**2) ** 0.5
+    def checkPinch(self, p1, p2, hand):
+        indexLen = self.dist(hand[7].x,hand[7].y,hand[6].x,hand[6].y)
+        thumbLen = self.dist(hand[4].x,hand[4].y,hand[3].x,hand[3].y)
+        #maxY = (indexLen + thumbLen) /2
+        maxY = 0.05
+        dx = abs(p1[0] - p2[0])
+        dy = abs(p1[1] - p2[1])
         dz = abs(p1[2] - p2[2])
-        print("xyDist: ", xyDist)
-        print("dz: ", dz)
-        #xyDist < 0.15 and 
-        return xyDist < 0.3 and dz < 0.08
+        #print(maxY)
+        #print("xyz Dist: ", round(dx,2), round(dy,2), round(dz,2))
+        if dx < 0.02 and dy < maxY and dz < 0.02:
+            self.pinch = True
+        else:
+            self.pinch = False
 
     def overThresh(self,startCoords, endCoords):
         if abs(startCoords[0] - endCoords[0]) > self.moveThresh or abs(startCoords[1] - endCoords[1]) > self.moveThresh:
@@ -78,24 +92,21 @@ class Hand:
         else:
             return False
 
+    def signedExp(self, base, exp):
+        if base < 0:
+            power = -(abs(base) ** exp)
+        else:
+            power = (abs(base) ** exp)
+        return power
+
     def mouseAcceleration(self, handLms, camSize, img):
         inputX = int(self.avg([handLms.landmark[item].x for item in [0, 9, 13]]) * camSize[0])
         inputY = int(self.avg([handLms.landmark[item].y for item in [0, 9, 13]]) * camSize[1])
         if (inputX > self.boundStart[0] and inputX < self.boundStart[0] + self.boundBox[0] 
         and inputY > self.boundStart[1] and inputY < self.boundStart[1] + self.boundBox[1]):
             self.inBounds = True
-            moveX = (inputX - self.prevInput[0]) * 2
-            moveY = (inputY - self.prevInput[1]) * 2
-            
-            if moveX < 0:
-                moveX = -(abs(moveX) ** self.acceleration)
-            else:
-                moveX = moveX ** self.acceleration
-            if moveY < 0:
-                moveY = -(abs(moveY) ** self.acceleration)
-            else:
-                moveY = moveY ** self.acceleration
-            
+            moveX = self.signedExp((inputX - self.prevInput[0]) * 1.2,self.acceleration)
+            moveY = self.signedExp((inputY - self.prevInput[1]) * 1.2,self.acceleration)
             mouseX = self.mouseCoords[0] + moveX
             mouseY = self.mouseCoords[1] + moveY
             if mouseX > self.display[0]:
@@ -110,26 +121,23 @@ class Hand:
             self.lastPos = self.queue(self.lastPos, self.mouseCoords)
             avgPos = self.avgPos(self.lastPos)
 
-            if self.overThresh(self.mouseCoords, avgPos):
+            if self.overThresh(self.mouseCoords, avgPos): #checks if movement is significant enough to not be noise
                 x =  int(self.mouseCoords[0] - avgPos[0])
                 y =  int(self.mouseCoords[1] - avgPos[1])
                 win32api.mouse_event(win32con.MOUSEEVENTF_MOVE, x, y, 0, 0) #necessary for centered mouse programs
                 #self.mouse.move(x, y)
                 #self.mouse.position = avgPos
-                
                 self.prevInput = (inputX, inputY)
         else:
             self.inBounds = False
-        
-        cv2.circle(img, (inputX, inputY), 3, (255, 255, 0), -1)  # draws hand position
+        cv2.circle(img, (inputX, inputY), 3, (255, 255, 0), - 1)  # draws position of the center of the palm
 
     def controlMouse(self):
-        #if self.fingersRaised[1:5] == [1,1,1,1]:# if 4 fingers, thumb not included, are raised
-        if self.pinch(self.thumbCoords, self.indexCoords):
+        if self.pinch:
             if self.leftDown == False:
                 self.mouse.press(Button.left)
                 self.leftDown = True
-                self.mouseAction = "Pinch"
+                self.mouseAction = "Hold Left"
         else:
             if self.leftDown == True:
                 self.leftDown = False
@@ -139,12 +147,12 @@ class Hand:
             if self.rightDown == False:
                 self.mouse.press(Button.right)
                 self.rightDown = True
-                self.mouseAction = "Right"
-        else:
-            if self.rightDown == True:
-                self.rightDown = False
-                self.mouse.release(Button.right)
-                self.mouseAction = "None"
+                self.mouseAction = "Hold Right"
+            else:
+                if self.rightDown == True:
+                    self.rightDown = False
+                    self.mouse.release(Button.right)
+                    self.mouseAction = "None"
             # index, middle and ring finger raised
             '''
             if self.fingersRaised[1:4] == [1, 1, 1]:
@@ -167,31 +175,35 @@ class Hand:
                     self.mouseAction = "Left Click"
                     self.lastClick = time.time()
 
-
     def draw(self):
         success, img = self.cap.read()  # tuple of boolean success and image feed
         img = cv2.flip(img,1) #inverts camera feed
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = self.hands.process(imgRGB)
         h, w, c = img.shape
+
         y_offset = 5  # vertical position offset for number label on finger landmarks
         if results.multi_hand_landmarks:
             for handLms in results.multi_hand_landmarks: #iterating through each hand
-                self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                if self.drawConnections:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
                 for id, lm in enumerate(handLms.landmark):
                     if self.drawLabels:
                         cv2.putText(img, str(id), (int(lm.x * w), int(lm.y * h - y_offset)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
                     if id % 4 == 0 and id > 4:
-                        if lm.y < handLms.landmark[id - 2].y:
+                        if (lm.y - handLms.landmark[id - 3].y) < - 0.15:
                             self.fingersRaised[id // 4 - 1] = 1
                         else:
                             self.fingersRaised[id // 4 - 1] = 0
 
-                self.indexCoords = [handLms.landmark[4].x, handLms.landmark[4].y, handLms.landmark[4].z]
-                self.thumb = [handLms.landmark[8].x, handLms.landmark[8].y, handLms.landmark[8].z]
+                indexCoords = [handLms.landmark[4].x, handLms.landmark[4].y, handLms.landmark[4].z]
+                thumbCoords = [handLms.landmark[8].x, handLms.landmark[8].y, handLms.landmark[8].z]
+                self.checkPinch(thumbCoords, indexCoords, handLms.landmark)
 
-                self.mouseAcceleration(handLms, (w, h), img)
-            if self.mouseRunning and self.inBounds:
+                if self.mouseRunning:
+                    self.mouseAcceleration(handLms, (w, h), img)
+            
+            if self.inBounds and self.mouseRunning and self.fingersRaised[1:5] != [1,1,1,1]:
                 self.controlMouse()
 
         cTime = time.time()
@@ -201,7 +213,8 @@ class Hand:
         self.pTime = cTime
 
         cv2.putText(img, avgFps, (10, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays fps
-        cv2.putText(img, self.mouseAction, (400, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays fps
+        cv2.putText(img, self.mouseAction, (400, 20), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays mouse action
+        cv2.putText(img, "Fingers: "+ str(self.fingersRaised), (400, 35), cv2.FONT_HERSHEY_SIMPLEX,0.5, (0, 255, 0), 2)  # displays mouse action
         cv2.rectangle(img, self.boundStart,( self.boundStart[0] + self.boundBox[0] , self.boundStart[1] + self.boundBox[1]), (0, 0, 255), 2) #draws virtual mousepad area
         cv2.imshow('Camera1', img)  # shows camera feed
         cv2.waitKey(1)  # waits for 1ms
--- main.py
+++ main.py
@@ -10,23 +10,27 @@ run = True
 def on_press(key):
     try:
         #print('alphanumeric key {0} pressed'.format(key.char))
-        if key.char == '/':  # toggles pauses the program
+        if key.char == '1':  # toggles pauses the program
             hand1.mouseRunning = not hand1.mouseRunning
+        elif key.char == '2':
+            hand1.drawLabels = not hand1.drawLabels
+        elif key.char == '3':
+            hand1.drawConnections = not hand1.drawConnections
+        elif key.char == '4':
+            hand1.drawLandmarks = not hand1.drawLandmarks
+
     except AttributeError:
         print('special key {0} pressed'.format(key))
-
 
 def on_release(key):
     print('{0} released'.format(key))
     if key == keyboard.Key.esc:  # stops the program
         os._exit(0)
-        hand1.release()
-
+        
 listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
 listener.start()
-
 
 def main():
     while True:
